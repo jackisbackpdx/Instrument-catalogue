45b32c7661f2dbe56ac43c0af094f42e31745118
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-07-23 12:33:42 -0700

    stupid errors destroying my life

--- src/generateInstrument.js
+++ src/generateInstrument.js
@@ -1,5 +1,5 @@
 function renderInstrument(instrument) {
-    const li = document.createElement('li');
+    let li = document.createElement('li');
     li.className = instrument.category;
     li.title = instrument.description;
 
@@ -15,17 +15,17 @@ function renderInstrument(instrument) {
     const p = document.createElement('p');
     p.className = 'price';
 
-    const usd = instrument.price.toLocaleString('en-US', { style: 'currency', curreency: 'USD'});
+    const usd = instrument.price.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
     const priceTextNode = document.createTextNode(usd);
     p.appendChild(priceTextNode);
 
     const button = document.createElement('button');
     button.textContent = 'Buy';
-    p.appendChild('button');
+    p.appendChild(button);
 
     li.appendChild(p);
 
-    return li
-};
+    return li;
+}
 
-export default renderFruit;
+export default renderInstrument;
--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,31 @@
+import renderInstrument from '../src/generateInstrument.js';
 const test = QUnit.test;
 
-test('time to test!', function(assert) {
+test('Renders an Instrument!', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-    const 
-    const expected = <li class="drums" title='Get loud and throw down with this drum set, so dope!'>
-    <h3>Drum Set</h3><img src='../assets/images/drumkit.jpeg'><p class="price">$200<button>Buy</button></p></li>
+    const drums = {
+        name: 'Drum Set',
+        code: 'Real Drums',
+        image: '../assets/images/drumkit.jpeg/',
+        description: 'Get loud and throw down with this drum set, so dope!',
+        category: 'drums',
+        price: 200.00,
+        cost: 125.00
+    };
 
-    result = 
+    const expected = '<li class="drums" title="Get loud and throw down with this drum set, so dope!"><h3>Drum Set</h3><img src="../assets/images/drumkit.jpeg"><p class="price">$200.00<button>Buy</button></p></li>';
+
+    
     
     //Act 
     // Call the function you're testing and set the result to a const
 
+    const dom = renderInstrument(drums);
+    const html = dom.outerHTML;
+
     //Assert
     // What should the result be?
 
-    assert.equal(true, false);
+    assert.equal(expected, html);
 });
