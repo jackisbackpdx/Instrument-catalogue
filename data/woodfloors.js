2152d1406e2bfcbea8d3fdb99ba5491681fca724
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-07-23 10:35:47 -0700

    made new repo with correct name

---
+++ data/woodfloors.js
@@ -0,0 +1,75 @@
+const drumset = {
+    name: 'Standard Drums',
+    code: 'Real Drums',
+    image: '../assets/images/drumkit.jpeg/',
+    description: 'Get loud and throw down with this awesome drum set, so dope!',
+    category: 'drums',
+    price: 200.00,
+    cost: 125.00
+};
+const electricDrums = {
+    name: 'Electronic Drums',
+    code: 'Electronic Drums',
+    image: '../assets/images/electronicdrums.jpg/',
+    description: 'Electronic drum beats without all of the noise',
+    category: 'drums',
+    price: 300.00,
+    cost: 200.00
+};
+const acousticGuitar = {
+    name: 'Acoustic Guitar',
+    code: 'Acoustic Guitar',
+    image: '../assets/images/acousticguitar.jpg/',
+    description: 'Play some classic tunes with this artetsianally crafted guitar',
+    category: 'guitars',
+    price: 240.00,
+    cost: 185.00
+};
+const electricGuitar = {
+    name: 'Electric Guitar',
+    code: 'Electric Guitar',
+    image: '../assets/images/electricguitar.jpg/',
+    description: 'Electric Guitar',
+    category: 'guitars',
+    price: 300.00,
+    cost: 200.00
+};
+const beginnerAudioInterface = {
+    name: 'Beginner Audio Interface',
+    code: 'focusrite',
+    image: '../assets/images/focusrite.jpg/',
+    description: 'Get started with recording real instruments',
+    category: 'Audio Interface',
+    price: 140.00,
+    cost: 100.00
+};
+const advancedAudioInterface = {
+    name: 'Advanced Audio Interface',
+    code: 'Focuswrong',
+    image: '../assets/images/electronicdrums.jpg/',
+    description: 'Connect instruments to your computer with this feature-ful device',
+    category: 'Audio Interface',
+    price: 215.00,
+    cost: 175.00
+};
+const drumPad = {
+    name: 'Drum Pad',
+    code: 'Drum Pad',
+    image: '../assets/images/drumpad.jpg/',
+    description: 'Tap the squares to play a simulated drum',
+    category: 'drums',
+    price: 150.00,
+    cost: 120
+};
+const instruments = [
+    drumset,
+    electricDrums,
+    acousticGuitar,
+    electricGuitar,
+    beginnerAudioInterface,
+    advancedAudioInterface,
+    drumPad
+];
+export default instruments;
+
+
